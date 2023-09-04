94e65a38c3aa97f170414c6164e59f5ed21e4de6
zhangz <zhangz@hiysg.com>
2023-09-04 23:16:55 +0800

    add

--- src/c_new.rs
+++ src/c_new.rs
@@ -2,10 +2,11 @@ use std::thread;
 use std::path::PathBuf;
 use std::io; 
 use vosk::{ Model, Recognizer };  
-use portaudio as pa;
+use std::collections::VecDeque;
+use weresocool_portaudio::{ PortAudio, DuplexStreamSettings, Stream  };
+use std::i16;
+use std::slice;
 
-const INTERLEAVED: bool = true;
-const LATENCY: pa::Time = 0.0; // Ignored by PortAudio::is_*_format_supported.
 const STANDARD_SAMPLE_RATES: [f64; 13] = [
     8000.0, 9600.0, 11025.0, 12000.0, 16000.0, 22050.0, 24000.0, 32000.0, 44100.0, 48000.0,
     88200.0, 96000.0, 192000.0,
@@ -14,68 +15,93 @@ const STANDARD_SAMPLE_RATES: [f64; 13] = [
 pub fn create() {
   let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
   config_path.push("model");
-
   // 加载模型  
   let model = Model::new(config_path.to_str().unwrap()).unwrap();  
   let mut voice = Recognizer::new(&model, 16000.0).unwrap();  
+  
+  let audio_self = PortAudio::new().unwrap();
+  
+  let settings = PortAudio::default_input_stream_settings::<i16>(
+        &audio_self,                       
+        1,
+        44100.0,            
+        16,              
+    ).unwrap();
 
-  let pa = pa::PortAudio::new().unwrap();
+  // 打开音频输入流
+  let mut stream = PortAudio::open_blocking_stream(&audio_self, settings).unwrap();
 
-    let num_devices = pa.device_count().unwrap();
-    println!("Number of devices = {}", num_devices);
+  // 创建一个i16类型的缓冲区来接收音频数据
+  // let mut buffer = vec![0u32; 256];
 
-    println!("Defualt input device: {:?}", pa.default_input_device());
-    println!("Defualt output device: {:?}", pa.default_output_device());
+  stream.start().unwrap();
 
-    println!("All devices:");
-    for device in pa.devices().unwrap() {
-        let (idx, info) = device.unwrap();
-        println!("--------------------------------------- {:?}", idx);
-        println!("{:#?}", &info);
+  loop {
+    let buf = stream.read(16).unwrap();
+    voice.accept_waveform(buf);  
+    let result = voice.result().single().unwrap(); 
+    println!("text: {}", result.text); 
+  }
+  
+  // stream.close().unwrap();  
 
-        let in_channels = info.max_input_channels;
-        let input_params = pa::StreamParameters::<i16>::new(idx, in_channels, INTERLEAVED, LATENCY);
-        let out_channels = info.max_output_channels;
-        let output_params =
-            pa::StreamParameters::<i16>::new(idx, out_channels, INTERLEAVED, LATENCY);
+  // let pa = pa::PortAudio::new().unwrap();
 
-        println!(
-            "Supported standard sample rates for half-duplex 16-bit {} channel input:",
-            in_channels
-        );
-        for &sample_rate in &STANDARD_SAMPLE_RATES {
-            if pa
-                .is_input_format_supported(input_params, sample_rate)
-                .is_ok()
-            {
-                println!("\t{}hz", sample_rate);
-            }
-        }
+  //   let num_devices = pa.device_count().unwrap();
+  //   println!("Number of devices = {}", num_devices);
 
-        println!(
-            "Supported standard sample rates for half-duplex 16-bit {} channel output:",
-            out_channels
-        );
-        for &sample_rate in &STANDARD_SAMPLE_RATES {
-            if pa
-                .is_output_format_supported(output_params, sample_rate)
-                .is_ok()
-            {
-                println!("\t{}hz", sample_rate);
-            }
-        }
+  //   println!("Defualt input device: {:?}", pa.default_input_device());
+  //   println!("Defualt output device: {:?}", pa.default_output_device());
 
-        println!("Supported standard sample rates for full-duplex 16-bit {} channel input, {} channel output:",
-                 in_channels, out_channels);
-        for &sample_rate in &STANDARD_SAMPLE_RATES {
-            if pa
-                .is_duplex_format_supported(input_params, output_params, sample_rate)
-                .is_ok()
-            {
-                println!("\t{}hz", sample_rate);
-            }
-        }
-    }
+  //   println!("All devices:");
+  //   for device in pa.devices().unwrap() {
+  //       let (idx, info) = device.unwrap();
+  //       println!("--------------------------------------- {:?}", idx);
+  //       println!("{:#?}", &info);
+
+  //       let in_channels = info.max_input_channels;
+  //       let input_params = pa::StreamParameters::<i16>::new(idx, in_channels, INTERLEAVED, LATENCY);
+  //       let out_channels = info.max_output_channels;
+  //       let output_params =
+  //           pa::StreamParameters::<i16>::new(idx, out_channels, INTERLEAVED, LATENCY);
+
+  //       println!(
+  //           "Supported standard sample rates for half-duplex 16-bit {} channel input:",
+  //           in_channels
+  //       );
+  //       for &sample_rate in &STANDARD_SAMPLE_RATES {
+  //           if pa
+  //               .is_input_format_supported(input_params, sample_rate)
+  //               .is_ok()
+  //           {
+  //               println!("\t{}hz", sample_rate);
+  //           }
+  //       }
+
+  //       println!(
+  //           "Supported standard sample rates for half-duplex 16-bit {} channel output:",
+  //           out_channels
+  //       );
+  //       for &sample_rate in &STANDARD_SAMPLE_RATES {
+  //           if pa
+  //               .is_output_format_supported(output_params, sample_rate)
+  //               .is_ok()
+  //           {
+  //               println!("\t{}hz", sample_rate);
+  //           }
+  //       }
+
+  //       println!("Supported standard sample rates for full-duplex 16-bit {} channel input, {} channel output:",
+  //                in_channels, out_channels);
+  //       for &sample_rate in &STANDARD_SAMPLE_RATES {
+  //           if pa
+  //               .is_duplex_format_supported(input_params, output_params, sample_rate)
+  //               .is_ok()
+  //           {
+  //               println!("\t{}hz", sample_rate);
+  //           }
+  //       }
+  //   }
 
   //   println!("请开始说话...");  
   // loop {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 pub mod pocket;
 pub mod tts;
 pub mod c_new;
-pub mod examp;
+// pub mod examp;
 
 fn main() -> Result<(), anyhow::Error> {
     // tts::tts_speak();
     // pocket::pocket_reader();
-    // c_new::create();
-    examp::run().unwrap();
+    c_new::create();
+    // examp::run().unwrap();
     Ok(())
 }
